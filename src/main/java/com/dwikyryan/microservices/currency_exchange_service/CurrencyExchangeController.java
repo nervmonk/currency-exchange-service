bb2c0ab795aebfc26fb2fb2a76853b4e7286d04b
dwikyryan <dwikyryan@gmail.com>
2024-10-16 23:49:48 +0700

    adding some

--- src/main/java/com/dwikyryan/microservices/currency_exchange_service/CurrencyExchangeController.java
+++ src/main/java/com/dwikyryan/microservices/currency_exchange_service/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.dwikyryan.microservices.currency_exchange_service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,18 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     private Environment environment;
     private CurrencyExchangeRepository currencyExchangeRepository;
 
-    public CurrencyExchangeController(Environment environment, CurrencyExchangeRepository currencyExchangeRepository){
+    public CurrencyExchangeController(Environment environment, CurrencyExchangeRepository currencyExchangeRepository) {
         this.environment = environment;
         this.currencyExchangeRepository = currencyExchangeRepository;
     }
-    
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
-    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
         var currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
-        if(currencyExchange == null){
+        if (currencyExchange == null) {
             throw new RuntimeException("Unable to find data");
         }
         currencyExchange.setEnvironment(environment.getProperty("local.server.port"));
